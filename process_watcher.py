c310efcd356478ebe6f37b806e1331bd13808e39
EveSquare <50450074+EveSquare@users.noreply.github.com>
2020-11-05 23:49:52 +0900

    Windows10

---
+++ process_watcher.py
@@ -0,0 +1,30 @@
+import subprocess
+import time
+# import joblib
+import msvcrt
+import sys
+
+count_minute = 0
+
+task_name = 'msedge'#Microsoft Edge
+
+def watch():
+    proc = subprocess.Popen('tasklist', shell=True, stdout=subprocess.PIPE)
+    for line in proc.stdout:
+        if not str(line).find(task_name) == -1:
+            print('active!')
+            return True
+
+    return False
+
+try:
+
+    while True:
+        if watch():
+            count_minute += 1
+        else:
+            print("not activated")
+
+        time.sleep(60)
+except KeyboardInterrupt:
+    sys.exit(0)
